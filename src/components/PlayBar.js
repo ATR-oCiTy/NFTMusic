6e3205581a44b4521d814c91cc84695bd861e43b
Arjun Shenoy <arjunshenoy23@gmail.com>
2022-05-01 12:38:34 +0530

    commit on 'main'

--- src/components/PlayBar.js
+++ src/components/PlayBar.js
@@ -1,48 +1,12 @@
-export default function PlayBar() {
+import ReactAudioPlayer from 'react-audio-player';
+export default function PlayBar({musicURL}) {
   return (
     <div className="footer-container">
-      <div className="music-detail">
-        <h1 className="song"> Something playing .... </h1>
-        <p className="singer"> Singer </p>
-      </div>
-      <div className="button-group">
-        <button>
-          <img src="/prev-button.svg" height="30px" width="30px" />
-        </button>
-        <button>
-          <img src="/pause-button.svg" height="30px" width="30px" />
-        </button>
-        <button>
-          <img src="/next-button.svg" height="30px" width="30px" />
-        </button>
-      </div>
-
-      <img src="/music-title.svg" height="100px" width="100px" />
-      <style jsx>{`
-        .footer-container {
-          height: 10vh;
-          margin-bottom: 1-10px;
-          border: 1px solid white;
-          display: flex;
-          justify-content: space-between;
-          background: rgba(0, 0, 0, 0.49);
-          opacity: 0.6;
-          z-index: 3;
-        }
-        .music-detail {
-          margin-top: auto;
-          margin-bottom: auto;
-          margin-left: 2%;
-          color: white;
-        }
-        .button-group {
-          margin-top: auto;
-          margin-bottom: auto;
-        }
-        button {
-          padding: 0.5em;
-        }
-      `}</style>
+    <ReactAudioPlayer
+      src={musicURL}
+      autoPlay
+      controls
+  />
     </div>
   );
 }
